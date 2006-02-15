1e49c0e3072b99370cc17436a65cca659d2f4edf
Rutger ter Borg <rutger@terborg.net>
2006-02-15 15:16:53 +0000

    adjusted the KRLS algorithm to the new framework

--- kml/krls.hpp
+++ kml/krls.hpp
@@ -21,13 +21,20 @@
 #ifndef KRLS_HPP
 #define KRLS_HPP
 
-#include <online_greedy_determinate.hpp>
+// #include <online_greedy_determinate.hpp>
+// #include "kernels.h"
 
+// #include <design_matrix.hpp>
+
+#include <boost/numeric/bindings/traits/ublas_matrix.hpp>
+#include <boost/numeric/bindings/traits/ublas_symmetric.hpp>
+#include <boost/numeric/bindings/traits/ublas_vector.hpp>
+
+#include <boost/numeric/ublas/matrix.hpp>
 #include <boost/numeric/bindings/atlas/cblas.hpp>
-#include "kernels.h"
-
-#include <design_matrix.hpp>
-
+#include <kml/kernel_machine.hpp>
+#include <kml/regression.hpp>
+#include <kml/symmetric_view.hpp>
 
 
 namespace atlas = boost::numeric::bindings::atlas;
@@ -38,15 +45,7 @@ namespace kml {
 
 /*!
 \brief Kernel Recursive Least Squares
-\param I the input type
-\param O the output type
-\param K the kernel type
    
-\todo
-- Perhaps this adding of a value to the kernel can be avoided if the same structure is used as is used by
-the on line_svm.
-- Migrate matrices to view_matrix (avoid resize costs)
-
 A preprocessing step is taken to absorb the bias term into the weight vector w, by
 redefining w as (w^T, b)^T and \f$\phi\f$ as (phi^T,1)^T. For details, see Engel et al,
 Sparse Online Greedy Support Vector Regression, below on page 3:
@@ -57,166 +56,195 @@ Sparse Online Greedy Support Vector Regression, below on page 3:
 */
 
 
-template<typename I, typename O, template<typename,int> class K>
-class krls: public online_greedy_determinate<I,O,K> {
+template< typename Problem, typename Kernel, typename PropertyMap, class Enable = void>
+class krls: public kernel_machine< Problem, Kernel, PropertyMap > {};
 
-    typedef online_greedy_determinate<I,O,K> base_type;
+
+
+template< typename Problem, typename Kernel, typename PropertyMap >
+class krls< Problem, Kernel, PropertyMap, typename boost::enable_if< is_regression<Problem> >::type>:
+         public kernel_machine< Problem, Kernel, PropertyMap > {
+
+	typedef kernel_machine< Problem, Kernel, PropertyMap > base_type;
     typedef typename base_type::kernel_type kernel_type;
     typedef double scalar_type;
-    typedef ublas::symmetric_matrix<double> symmetric_type;
+
+    typedef symmetric_view< ublas::matrix<double> > symmetric_type;
+        
     typedef ublas::matrix<double> matrix_type;
     typedef ublas::vector<double> vector_type;
-    typedef I input_type;
-    typedef O output_type;
-    
+
+    typedef typename Problem::input_type input_type;
+    typedef typename Problem::output_type output_type;
+
+	typedef typename boost::property_traits<PropertyMap>::key_type key_type;
+	typedef typename boost::property_traits<PropertyMap>::value_type object_type;
 
 public:
-    krls( typename boost::call_traits<kernel_type>::param_type k, scalar_type n=1e-1, scalar_type l=1e-3 ):
-            base_type(k), nu(n), lambda_squared(l*l) {}
+    krls( scalar_type n, scalar_type l, 
+    		typename boost::call_traits<kernel_type>::param_type k,  
+        	typename boost::call_traits<PropertyMap>::param_type map ):
+            base_type(k,map), nu(n), lambda_squared(l*l) {}
+            
+    template< typename TokenIterator >
+    krls( TokenIterator const begin, TokenIterator const end, 
+         typename boost::call_traits<kernel_type>::param_type k,
+         typename boost::call_traits<PropertyMap>::param_type map ):
+		base_type(k,map) {
+		nu = 1e-1;						// default value
+		scalar_type lambda=1e-3;		// default value
+		TokenIterator token(begin);
+		if ( token != end ) {
+			nu = boost::lexical_cast<double>( *token++ );
+			if ( token != end ) lambda = boost::lexical_cast<double>( *token );
+		}
+		lambda_squared=lambda*lambda;
+	}
+            
 
     output_type operator()( input_type const &x ) {
     
-        // TODO do a trick here::: replace the base_type::kernel with another functor,
-	// but then one with the value added. Then, dispatch to the underlying base_type::kernel machine.
-	vector_type temp_K( base_type::support_vector.size() );
-        for( int i=0; i < base_type::support_vector.size(); ++i )
-           temp_K[i] = base_type::kernel( base_type::support_vector[i], x ) + lambda_squared;
-        
+		vector_type temp_K( base_type::key_lookup.size() );
+		base_type::fill_kernel( x, base_type::key_lookup.begin(), base_type::key_lookup.end(), temp_K.begin() );
+		for( int i=0; i < temp_K.size(); ++i )
+           temp_K[i] += lambda_squared;
         return atlas::dot( base_type::weight, temp_K );
 
-/*	
-	// matrix_type temp_H;
-        base_type::kernel_matrix( base_type::support_vector, X, base_type::kernel, temp_H );
-	if (base_type::kernel.order==0) {
-	for( int i = 0; i<temp_H.size1(); ++i )
-            for( int j = 0; j<temp_H.size2(); ++j )
-                temp_H(i,j) += lambda_squared;
-	}
-        atlas::gemv( temp_H, base_type::weight, result );*/
-    
     }
     
-    
-    /*! \param x_t an input pattern
-        \param y_t an output pattern */
-    void push_back( input_type const &x_t, output_type const &y_t ) {
 
-	//std::cout << "running point " << x_t << " through KRLS..." << std::endl;
+   /*! learn the entire range of keys indicated by this range */
+    template<typename KeyIterator>
+    void learn( KeyIterator begin, KeyIterator end ) {
+		KeyIterator key_iterator(begin);
+		while( key_iterator != end ) {
+			increment( *key_iterator );
+			++key_iterator;
+		}
+    }
     
-	// calculate the base_type::kernel function on (x_t,x_t), needed later on
-        scalar_type k_tt = base_type::kernel( x_t, x_t ) + lambda_squared;
+        
+    /*! \param key key of example in data */
+    void increment( key_type const &key ) {
+	    
+	    //std::cout << "running key " << key << " through KRLS... " << std::endl;
+    
+		// calculate the base_type::kernel function on (x_t,x_t), needed later on
+        scalar_type k_tt = base_type::kernel( key, key ) + lambda_squared;
 
         // check whether dictionary is still not initialised
-        if ( base_type::support_vector.size() == 0 ) {
+        if ( base_type::key_lookup.empty() ) {
 
-	    // there is no dictionary yet, so initialise all variables
-            // resize the matrix H, its inverse Hinv and matrix P to 1 x 1
-            H.resize(1);
-            R.resize(1);
-            P.resize(1);
+	    	// there is no dictionary yet, so initialise all variables
+            // resize the matrix K, its inverse R and matrix P to 1 x 1
+            K.grow_row_column();
+            R.grow_row_column();
+            P.grow_row_column();
 
             // and use values as stated in the paper
-            H(0,0) = k_tt;
-            R(0,0) = 1.0 / k_tt;
-            P(0,0) = 1.0;
+            K.matrix(0,0) = k_tt;
+            R.matrix(0,0) = 1.0 / k_tt;
+            P.matrix(0,0) = 1.0;
 
             // add to weight vector
-	    base_type::weight.push_back( y_t / k_tt );
+		    base_type::weight.push_back( (*base_type::data)[key].second / k_tt );
 
             // add to support vector set
-            base_type::support_vector.push_back( x_t );
+            base_type::key_lookup.push_back( key );
 
         } else {
 
             // KRLS already initialised, continue
-            vector_type a_t( H.size1() );
-            vector_type k_t( H.size1() );
+            vector_type a_t( K.size1() );
+            vector_type k_t( K.size1() );
 
-	    // fill vector k_t
-	    for( int i=0; i<base_type::support_vector.size(); ++i )
-	       k_t[i] = base_type::kernel( base_type::support_vector[i], x_t ) + lambda_squared;
+		    // fill vector k_t
+		    base_type::fill_kernel( key, base_type::key_lookup.begin(), base_type::key_lookup.end(), k_t.begin() );
+		    for( int i=0; i<k_t.size(); ++i ) k_t[i] += lambda_squared;
 
-            atlas::spmv( R, k_t, a_t );
+			// a_t <- R %*% k_t
+            ublas::matrix_range< ublas::matrix<double> > R_range( R.view() );
+            ublas::symmetric_adaptor< ublas::matrix_range< ublas::matrix<double> > > R_view( R_range );
+            atlas::symv( R_view, k_t, a_t );
             scalar_type delta_t = k_tt - atlas::dot( k_t, a_t );
 
-            // do ALD test
+            // Perform Approximate Linear Dependency (ALD) test
             if (delta_t > nu) {
+	            
                 // add x_t to support vector set, adjust all needed variables
-                unsigned int old_size = base_type::support_vector.size();
+                unsigned int old_size = base_type::key_lookup.size();
                 unsigned int new_size = old_size + 1;
-
-                // update H (equation 14)
-                preserved_resize( H, new_size, new_size );
-                ublas::matrix_vector_slice<symmetric_type> H_row_part( H, ublas::slice(old_size,0,old_size),
-                        ublas::slice(0,1,old_size) );
-                H_row_part.assign( k_t );
-                H( old_size, old_size ) = k_tt;
-
+                
+				// update K (equation 14)
+                // fetch a view into the last row of the matrix of the _old_ size
+				K.grow_row_column();
+ 			    ublas::matrix_vector_slice< ublas::matrix<double> > K_row_part( K.shrinked_row(old_size) );
+ 			    atlas::copy( k_t, K_row_part );
+                K.matrix( old_size, old_size ) = k_tt;
+				
                 // update R (equation 14)
                 scalar_type factor = static_cast<scalar_type>(1) / delta_t;
-                atlas::spr( factor, a_t, R );
-                preserved_resize( R, new_size, new_size );
-                ublas::matrix_vector_slice<symmetric_type> R_row_part( R, ublas::slice(old_size,0,old_size),
-                        ublas::slice(0,1,old_size) );
+                atlas::syr( factor, a_t, R_view );
+                R.grow_row_column();
+			    ublas::matrix_vector_slice< ublas::matrix<double> > R_row_part( R.shrinked_row(old_size) );
                 atlas::scal( -factor, a_t );
                 R_row_part.assign( a_t );
-                R( old_size, old_size ) = factor;
-
+                R.matrix( old_size, old_size ) = factor;
+                
                 // update P (equation 15)
                 // assign unit vector with 1 on last element.
-                preserved_resize( P, new_size, new_size );
-                ublas::matrix_row<symmetric_type> P_row( P, old_size );
-                // NOTE TODO workaround for compiler / ublas!
-                //P_row.assign_temporary( ublas::unit_vector<scalar_type>( new_size, old_size ) );
-                std::fill(P_row.begin(), P_row.end(), 0.0 );
-                P_row( old_size ) = 1.0;
+                P.grow_row_column();
+				ublas::matrix_vector_slice< ublas::matrix<double> > P_row_part( P.shrinked_row(old_size) );                
+				atlas::set(  0.0, P_row_part );
+				P.matrix( old_size, old_size ) = 1.0;
 
                 // adjust weight vector alpha (equation 16)
-                factor = y_t-atlas::dot(k_t,base_type::weight);
+                factor = (*base_type::data)[key].second - atlas::dot(k_t,base_type::weight);
                 atlas::axpy( factor, a_t, base_type::weight );
 		
-		// add new weight to the weight vector
+				// add new weight to the weight vector
                 base_type::weight.push_back( factor / delta_t );
 
-                // add support vector to the set, add 1 row
-	        base_type::support_vector.push_back( x_t );
+                // add support vector to the set
+	        	base_type::key_lookup.push_back( key );
 
             } else {
                 // support vector set unchanged (see algorithmic on page 4 of paper)
                 // adjust weight vector and permutation matrix P
                 // P_a <- P_t-1 %*% a_t
-                vector_type P_a( base_type::support_vector.size() );
+                vector_type P_a( base_type::key_lookup.size() );
+
                 // spmv(A,x,y)       y <- A x
-                atlas::spmv( P, a_t, P_a );
-                // 1 / (1 + a_t %*% P_t-1 %*% a)
+    	        ublas::matrix_range< ublas::matrix<double> > P_range( P.view() );
+	            ublas::symmetric_adaptor< ublas::matrix_range< ublas::matrix<double> > > P_view( P_range );
+                atlas::symv( P_view, a_t, P_a );
+
+                // 1 / (1 + a_t %*% P_(t-1) %*% a)
                 scalar_type factor = 1.0 / (1.0 + atlas::dot( a_t, P_a ));
 
-                // update base_type::weight (equation 13)
-                atlas::spmv( factor*(y_t-atlas::dot(k_t,base_type::weight)), R, P_a, static_cast<scalar_type>(1), base_type::weight );
+                // update weights (equation 13)
+                atlas::symv( factor* (*base_type::data)[key].second - atlas::dot(k_t,base_type::weight), 
+                			 R_view, P_a, static_cast<scalar_type>(1), base_type::weight );
 
                 // update permutation matrix (equation 14)
-                atlas::spr( -factor, P_a, P );
-
-            }
+                atlas::syr( -factor, P_a, P_view );
+			}
         }
     }
 
 
-
-
-
 private:
-    // method specific local memory
-    symmetric_type H;                // base_type::kernel matrix K
-    symmetric_type R;                // inverse of base_type::kernel matrix K
+
+    symmetric_type K;                // kernel matrix K
+    symmetric_type R;                // inverse of kernel matrix K
     symmetric_type P;                // permutation matrix P
-    scalar_type nu;                   // ALD parameter
-    scalar_type lambda_squared;       // base_type::kernel function addition
+    scalar_type nu;                  // ALD parameter
+    scalar_type lambda_squared;      // kernel function addition
 };
 
 
 
-} // namespace
+} // namespace kml
 
 #endif
 
