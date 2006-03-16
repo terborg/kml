0ee141ba8ee93459459c6e655f9aa0cfbdd650e1
Meredith Patterson <mlp@thesmartpolitenerd.com>
2006-03-16 11:20:08 +0000

    fixed! compiles now.

--- example/svm_classification.cpp
+++ example/svm_classification.cpp
@@ -36,10 +36,8 @@ namespace ublas = boost::numeric::ublas;
 int main(int argc, char *argv[]) {
 
   typedef boost::tuple<std::vector<double>, bool> example_type;
-  typedef boost::tuples::element<0, example_type>::type input_type;
   typedef kml::classification<example_type> problem_type;
   typedef boost::vector_property_map<example_type> data_type;
-  typedef std::vector<double>::iterator vec_iter;
   typedef kml::gaussian<problem_type::input_type> kernel_type;
 
   if (argc < 2) {
@@ -51,14 +49,13 @@ int main(int argc, char *argv[]) {
   BOOST_STATIC_ASSERT((boost::is_same<boost::property_traits<data_type>::value_type, example_type >::type::value));
 
   data_type data;
-  std::vector<input_type> learn_keys;
+  std::vector<data_type::key_type> learn_keys;
   cerr << "Reading in vector...";
   kml::file train_file(argv[1]);
   train_file.read(data, learn_keys);
   
-  cerr << "Done, " << std::distance(learn_keys.begin(), learn_keys.end()) << " points of size " << learn_keys[0].size() << endl;
+  cerr << "Done, " << std::distance(learn_keys.begin(), learn_keys.end()) << " points of size " << data[0].get<0>().size() << endl;
   kml::svm<problem_type, kernel_type, data_type> my_machine(3.162277, 1.0, data);
-
 
   my_machine.learn(learn_keys.begin(), learn_keys.end());
   cerr << "Done training" << endl;
@@ -68,12 +65,12 @@ int main(int argc, char *argv[]) {
   cout << endl;
 
   data_type test_data;
-  std::vector<input_type> test_keys;
-  std::vector<input_type>::iterator i;
+  std::vector<data_type::key_type> test_keys;
+  std::vector<data_type::key_type>::iterator i;
   cerr << "Reading in test vector...";
   kml::file test_file(argv[2]);
-  cerr << "Done, " << std::distance(test_keys.begin(), test_keys.end()) << " points of size " << test_keys[0].size() << endl;
+  cerr << "Done, " << std::distance(test_keys.begin(), test_keys.end()) << " points of size " << test_data[0].get<0>().size() << endl;
   for (i = test_keys.begin(); i != test_keys.end(); ++i)
-    cout << my_machine(*i) << endl;
+    cout << my_machine(test_data[*i].get<0>()) << endl;
   return 0;
 }
